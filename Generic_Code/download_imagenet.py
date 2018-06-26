c2f7b5a2e48c912a1d243662fc9c9efa3cf70925
Zachary Stillman <zjstillman@berkeley.edu>
2018-06-26 14:29:55 -0700

    Added downloading for imagenet files

---
+++ Generic_Code/download_imagenet.py
@@ -0,0 +1,18 @@
+import urllib.request
+import urllib
+import os
+
+file = open("fall11_urls.txt", "r")
+i = 0
+while i < 1000:
+	try:
+		f = open('imagenet/' + str(i) + '.jpg','wb')
+		f.write(urllib.request.urlopen(file.readline().split()[1]).read())
+		f.close()
+		statinfo = os.stat('imagenet/' + str(i) + '.jpg')
+		if statinfo.st_size > 0:
+			i += 1
+		else:
+			print('Empty: ' + str(i))
+	except:
+		print('Failed: ' + str(i))
